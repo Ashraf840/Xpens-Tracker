d8d0df068960bfa530ca2a9b69d341a2b127ff4e
Tanjim <shift2unleashed2011@gmail.com>
2021-01-31 19:16:22 +0600

    Edit Category Functionality

--- expensesapp/urls.py
+++ expensesapp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('add-expense/', views.addExpense, name='createNewExpense'),
     path('update-expense/<int:id>/', views.updateExpense, name='updateExpense'),
     path('delete-expense/<int:id>/', views.deleteExpense, name='deleteExpense'),
+    
     path('category-list/', views.categoryList, name='categorylist'),
     path('add-category/', views.addCategory, name='createNewCategory'),
+    path('update-category/<int:id>/', views.updateCategory, name='updateCategory'),
 ]
